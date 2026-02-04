014b82a3e59c5de611e852a750181dc6f182d60d
prashant prajul <prajulofficial3125@gmail.com>
2026-02-04 21:41:29 +0530

    Moore's Voting algorithm for >n/2 && >n/3 times

--- Linear/Array/Algorithms/MooreVotingAlgo.cpp
+++ Linear/Array/Algorithms/MooreVotingAlgo.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
 
-int MooresVotingAlgorithm(vector<int> nums)
+//NOTE : We cant find Elements exists exactly n/2 times
+
+//If Element exists more than n/2 times
+vector<int> MooresVotingAlgorithm(vector<int> nums)
 {
     int cnt=0, curr=INT_MIN;
     for(auto it : nums)
@@ -13,27 +16,67 @@ int MooresVotingAlgorithm(vector<int> nums)
         else
             cnt--;
     }
-    return curr;
+    return {curr};
 }
 
-//If Element exists only n/2 times
-//INCOMPLETE 
-int MooresVotingAlgorithm2(vector<int> nums)
+//If Element exists more than n/3 times
+vector<int> MooresVotingAlgorithm3(vector<int> nums) //
 {
-    int cnt=0 ,curr=INT_MIN;
+    int n =nums.size();
+    n/=3;
+    ++n;
+
+    int cnt1=0, curr1=INT_MIN;
+    int cnt2=0, curr2=INT_MIN;
     for(auto it : nums)
     {
-        if(cnt == 0)
+        if(cnt1 == 0 && it != curr2)
         {
-
+            curr1 = it;
+            cnt1++;
         }
-        if(it == curr)
-            cnt++;
+        else if(cnt2 == 0 && it != curr1)
+        {
+            curr2 = it;
+            cnt2++;
+        }
+        else if(it == curr1)
+        {
+            cnt1++;
+        }
+        else if(it == curr2)
+        {
+            cnt2++;
+        }
         else
-            cnt--;
+        {
+            cnt1--;
+            cnt2--;
+        }
     }
-    return curr;
+
+    cnt1 = 0;
+    cnt2 = 0;
+
+    for(auto it : nums)
+    {
+        if(it == curr1)
+            cnt1++;
+        if(it == curr2)
+            cnt2++;
+    }
+
+    vector<int> ans;
+
+    if(cnt1 >= n)
+        ans.push_back(curr1);
+    if(cnt2 >= n)
+        ans.push_back(curr2);
+
+    return ans;
 }
+
+
 int main()
 {
     // int n;
@@ -46,6 +89,12 @@ int main()
     //     cin>>k;
     //     nums.push_back(k);
     // }
-    cout<<MooresVotingAlgorithm(nums);//Exists n/2 + 1 times
-    // cout<<MooresVotingAlgorithm2(nums);
+    vector<int> ans;
+
+    ans = MooresVotingAlgorithm(nums);
+        cout<<"Elements occuring greater than n/2 times"<<ans[0];
+
+    ans = MooresVotingAlgorithm3(nums);
+    for(auto it : ans)
+        cout<<"Elements occuring greater than n/3 times"<<ans[0]<<" "<<ans[1];
 }
